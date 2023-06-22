7a58bf2aa243864f1e8dc2e5e1f9da698097b8c4
Vantrex <vantrexplays@gmail.com>
2023-06-22 19:15:54 +0200

    Initial Commit

---
+++ src/main/java/net/drapuria/minecraft/rest/DrapuriaMinecraftRestApplication.java
@@ -0,0 +1,13 @@
+package net.drapuria.minecraft.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrapuriaMinecraftRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrapuriaMinecraftRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/drapuria/minecraft/rest/DrapuriaMinecraftRestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.drapuria.minecraft.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrapuriaMinecraftRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
